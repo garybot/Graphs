4c314359a3b6b0583ead91028492dfd35c6c8fcb
garybot <argot.pryer@gmail.com>
2020-09-24 20:57:34 -0700

    A working version!

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,6 +5,9 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Queue
+
+
 # Load world
 world = World()
 
@@ -28,6 +31,85 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+adjacency = {}
+back_path = {"n": "s", "e": "w", "s": "n", "w": "e"}
+
+def has_unexplored_exits(room):
+    return "?" in adjacency[room].values()
+
+def DFS():
+    while has_unexplored_exits(player.current_room.id):
+        # choose an unexplored direction
+        directions = player.current_room.get_exits()
+        unexplored = [d for d in directions if adjacency[player.current_room.id][d] == "?"]
+        direction = random.choice(unexplored)
+
+        # travel
+        last_room = player.current_room.id
+        traversal_path.append(direction)
+        player.travel(direction)
+        current_room = player.current_room.id
+
+        # update adjacency list
+        adjacency[last_room][direction] = current_room
+        add_to_visited()
+        adjacency[current_room][back_path[direction]] = last_room
+
+def find_unexplored():
+    # BFS, recursive?
+    # current = player.current_room.id
+    # destination = any room with unexplored exits!
+    # make a queue?
+    q = Queue()
+    q.enqueue([player.current_room.id])
+    visited = set()
+    # store the paths?
+    paths = {player.current_room.id: []}
+    while q.size():
+        current = q.dequeue()
+
+        if has_unexplored_exits(current[-1]):
+            return paths[current[-1]]
+
+        if current[-1] not in visited:
+            visited.add(current[-1])
+
+            for direction in adjacency[current[-1]]:
+                q.enqueue([current] + [adjacency[current[-1]][direction]])
+                paths[adjacency[current[-1]][direction]] = paths[current[-1]] + [direction]
+    return []
+
+
+def add_to_visited():
+    # If the current room is not in the adjacency list, add it.
+    if not adjacency.get(player.current_room.id):
+        adjacency[player.current_room.id] = {}
+        for direction in player.current_room.get_exits():
+            adjacency[player.current_room.id][direction] = "?"
+
+def traverse():
+    add_to_visited()
+    while len(adjacency) < len(world.rooms):
+        # print(len(adjacency), len(world.rooms))
+        # print(adjacency)
+        DFS()  # Find a dead end
+        # Find an unexplored exit
+        path = find_unexplored()
+        for direction in path:
+            # travel
+            last_room = player.current_room.id
+            traversal_path.append(direction)
+            player.travel(direction)
+            current_room = player.current_room.id
+
+            # update adjacency list
+            adjacency[last_room][direction] = current_room
+            add_to_visited()
+            adjacency[current_room][back_path[direction]] = last_room
+        
+    # print(adjacency)
+    # print(traversal_path)
+traverse()
 
 
 
@@ -51,12 +133,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
